9b35c32172e99db9209987de4cd4cd604aecbbb9
ayushiaks <f2016587@hyderabad.bits-pilani.ac.in>
2018-02-01 08:00:15 +0530

    contactus: Add contact us section.

--- app/urls.py
+++ app/urls.py
@@ -3,6 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),	
+    path('', views.home, name='home'),
     path('app/<int:user_id>/', views.show_user_info),
+    path('contact/', views.contact, name='contact'),
 ]
--- app/views.py
+++ app/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import User
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
-
+from django.template import RequestContext
 from django.http import HttpResponse
 
 @login_required
@@ -25,3 +25,10 @@ def show_user_info(request, user_id):
     # Browse /app/<user_id>
     user = User.objects.get(id=user_id)
     return render(request, 'sample_template.html', {'user':user})
+
+def contact(request):
+	#user = User.objects.get(id=user_id)
+	context = RequestContext(request)
+	contact_list = User.objects.filter(is_staff=True)
+	context_dict = {'members': contact_list}
+	return render(request, 'contact.html', context_dict, context)
