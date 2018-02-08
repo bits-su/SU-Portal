df24804862eb93f2b37bf9f23eeb36d7a84348a7
KaitouKid1412 <vsurya.ayyagari@gmail.com>
2018-02-08 18:34:26 +0530

    Fix URL bug. I won't specify which bug cause I want you all to suffer.

--- simpleticket/urls.py
+++ simpleticket/urls.py
@@ -1,16 +1,16 @@
 from django.conf.urls import include
 from django.urls import path
-from . import views
+from simpleticket import views
 
 urlpatterns = [
     path('', views.view_all),
-    path('view/(<ticket_id>\d+)/', views.view),
+    path('view/<int:ticket_id>/', views.view),
     path('new/', views.create),
     path('submit_ticket/', views.submit_ticket),
-    path('update/(<ticket_id>\d+)/', views.update),
-    path('update_ticket/(<ticket_id>\d+)/', views.update_ticket),
-    path('submit_comment/(<ticket_id>\d+)/', views.submit_comment),
-    path('delete/(<ticket_id>\d+)/', views.delete_ticket),
-    path('delete_comment/(<comment_id>\d+)/', views.delete_comment),
+    path('update/<int:ticket_id>/', views.update),
+    path('update_ticket/<int:ticket_id>/', views.update_ticket),
+    path('submit_comment/<int:ticket_id>/', views.submit_comment),
+    path('delete/(<int:ticket_id>\d+)/', views.delete_ticket),
+    path('delete_comment/<int:comment_id>/', views.delete_comment),
     path('project/', views.project),
 ]
