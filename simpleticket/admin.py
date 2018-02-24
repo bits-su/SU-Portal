fbc146bf71ae983d92ac8ac833fd3fbcac808f5e
Basu Dubey <basu.96@gmail.com>
2018-02-24 21:12:29 +0530

    admin: Admin tweaks

--- simpleticket/admin.py
+++ simpleticket/admin.py
@@ -24,6 +24,30 @@ class TicketAdmin(admin.ModelAdmin):
     model = Ticket
     inlines = [TicketCommentInLine]
 
+    def get_logger(self,Ticket):
+        if(Ticket.created_by.username == 'pseudo'):
+            return('anonymous')
+        else:
+            username = Ticket.created_by.username
+            name = Ticket.created_by.first_name + Ticket.created_by.last_name
+            return(username + ": " + name)
+
+    list_display = [
+        'id',
+        'name',
+        'desc',
+        'status',
+        'get_logger',
+    ]
+
+    list_display_links = [
+        'id',
+        'name',
+        'desc',
+    ]
+
+    list_editable = ['status']
+
 
 admin.site.register(Project, ProjectAdmin)
 admin.site.register(Priority, PriorityAdmin)
